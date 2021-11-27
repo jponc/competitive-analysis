ccea3afb5bdf4d582c4bbac5390a2c567d49ed9a
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2021-11-27 18:31:02 +1100

    Added the remaining tables

--- migrations/list.ts
+++ migrations/list.ts
@@ -28,6 +28,52 @@ const migrations = {
       ALTER TABLE query_job ALTER COLUMN created_at SET DEFAULT NOW();
     `);
   },
+  v03_create_query_location: async (client: Client) => {
+    await client.query(`
+      CREATE TABLE query_location
+        (
+           id            UUID DEFAULT uuid_generate_v4(),
+           query_job_id  UUID NOT NULL,
+           device        TEXT NOT NULL,
+           search_engine TEXT NOT NULL,
+           num           INTEGER NOT NULL,
+           country       TEXT NOT NULL,
+           location      TEXT NOT NULL,
+           created_at    TIMESTAMP NOT NULL DEFAULT NOW(),
+           PRIMARY KEY(id),
+           CONSTRAINT fk_query_job FOREIGN KEY(query_job_id) REFERENCES query_job(id)
+        );
+    `);
+  },
+  v04_create_query_item: async (client: Client) => {
+    await client.query(`
+      CREATE TABLE query_item
+        (
+           id                 UUID DEFAULT uuid_generate_v4(),
+           query_job_id       UUID NOT NULL,
+           query_location_id  UUID NOT NULL,
+           position           INTEGER NOT NULL,
+           url                TEXT NOT NULL,
+           title              TEXT NOT NULL,
+           body               TEXT NOT NULL,
+           processed_at       TIMESTAMP,
+           created_at         TIMESTAMP NOT NULL DEFAULT NOW(),
+           PRIMARY KEY(id),
+           CONSTRAINT fk_query_job FOREIGN KEY(query_job_id) REFERENCES query_job(id),
+           CONSTRAINT fk_query_location FOREIGN KEY (query_location_id) REFERENCES query_location (id)
+        );
+    `);
+  },
+  v05_add_zenserp_batch_id_to_query_job: async (client: Client) => {
+    await client.query(`
+      ALTER TABLE query_job ADD COLUMN zenserp_batch_id TEXT;
+    `);
+  },
+  v06_update_num_to_text: async (client: Client) => {
+    await client.query(`
+      ALTER TABLE query_location ALTER COLUMN num TYPE TEXT;
+    `);
+  },
 };
 
 export default migrations;
